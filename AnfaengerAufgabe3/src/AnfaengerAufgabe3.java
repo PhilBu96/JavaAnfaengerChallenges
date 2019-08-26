146829750d46b10224ad6535a747798fd3ec56c6
Philipp Buthmann <nolimitsfan@gmail.com>
2019-08-26 21:45:18 +0200

    Added solution

--- AnfaengerAufgabe3/src/AnfaengerAufgabe3.java
+++ AnfaengerAufgabe3/src/AnfaengerAufgabe3.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
+
 public class AnfaengerAufgabe3 {
 
     public static void main(String[] args) {
 
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bitte etwas eintippen:\n");
+        System.out.println(sc.nextLine());
 
     }
 
